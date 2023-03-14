a0bbbcb5847e01ab4bf77c7dabee70bd03f35f70
Lasse Benninga <devops.pipeline@example.com>
2023-03-14 23:18:20 +0100

    Check for changes

--- scripts/load_s3_files.py
+++ scripts/load_s3_files.py
@@ -1,10 +1,18 @@
+import hashlib
 import argparse
+
 import s3fs
 
 file_name = "dbt.duckdb"
 bucket_name = "lbenninga-projects"
 path = f"ticketswap/dbt/{file_name}"
 
+def calculate_md5(file_path):
+    md5_hash = hashlib.md5()
+    with open(file_path, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            md5_hash.update(chunk)
+    return md5_hash.hexdigest()
 
 def download_file(bucket_name, s3_source_path, local_path):
     fs = s3fs.S3FileSystem()
@@ -13,12 +21,17 @@ def download_file(bucket_name, s3_source_path, local_path):
             local_file.write(s3_file.read())
     print('File downloaded from S3 to local directory.')
 
-def upload_file(bucket_name, local_path, s3_destination_path):
-    fs = s3fs.S3FileSystem()
-    with open(local_path, 'rb') as local_file:
-        with fs.open(f's3://{bucket_name}/{s3_destination_path}', 'wb') as s3_file:
-            s3_file.write(local_file.read())
-    print('File uploaded from local directory to S3.')
+def upload_file(bucket_name, local_path, s3_destination_path, original_md5):
+    current_md5 = calculate_md5(local_path)
+
+    if current_md5 != original_md5:
+        fs = s3fs.S3FileSystem()
+        with open(local_path, 'rb') as local_file:
+            with fs.open(f's3://{bucket_name}/{s3_destination_path}', 'wb') as s3_file:
+                s3_file.write(local_file.read())
+        print('File uploaded from local directory to S3.')
+    else:
+        print('File has not changed. Skipping upload.')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Download and upload files to/from Amazon S3.')
@@ -29,12 +42,15 @@ if __name__ == '__main__':
 
     # Upload command
     upload_parser = subparsers.add_parser('upload', help='Upload a file from local directory to S3.')
+    upload_parser.add_argument('original_md5', help='The original MD5 hash of the downloaded file.')
 
     args = parser.parse_args()
 
     if args.command == 'download':
-        download_file(bucket_name=bucket_name, s3_source_path=path, local_path=file_name)
+        hash = download_file(bucket_name=bucket_name, s3_source_path=path, local_path=file_name)
+        original_md5 = calculate_md5(file_name)
+        print(original_md5)
     elif args.command == 'upload':
-        upload_file(bucket_name=bucket_name, local_path=file_name, s3_destination_path=path)
+        upload_file(bucket_name=bucket_name, local_path=file_name, s3_destination_path=path, original_md5=args.original_md5)
     else:
         parser.print_help()
