2b666a005bae2716cd9c39260b7347972182d2f2
Lasse Benninga <devops.pipeline@example.com>
2023-03-17 10:25:42 +0100

    Remove rill

--- rill/models/median_ticket_price_count.sql
+++
@@ -1,7 +0,0 @@
-select event_name, entrance_title, max(updated), count(price) as prices_count, median(price) as median_price from mart_tickets_enriched
-where price =
-(
-  select price from (select event_name, entrance_title, median(price) from mart_tickets_enriched
-  group by 1,2)
-  )
-group by 1,2
