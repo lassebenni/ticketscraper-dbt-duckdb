42f7db18d1d8097882b73bad53b55c0bb972e855
Lasse Benninga <devops.pipeline@example.com>
2023-03-20 21:11:00 +0100

    format using sqlfluff

--- models/int/int_seller.sql
+++ models/int/int_seller.sql
@@ -1,15 +1,16 @@
 {{ config(materialized="table") }}
 
 with
-    tickets_sold as (select * from {{ ref("stg_tickets_sold") }}),
+tickets_sold as (select * from {{ ref("stg_tickets_sold") }}
+),
 
-    seller as (
-        select
-            seller_name as name,
-            seller_id as id
+seller as (
+    select
+        seller_name as name,
+        seller_id as id
 
-        from tickets_sold
-    )
+    from tickets_sold
+)
 
 select *
 from seller
--- models/marts/mart_scraped_events.sql
+++ models/marts/mart_scraped_events.sql
@@ -2,46 +2,49 @@
 {{ config(materialized='external', format='parquet', location="s3://lbenninga-projects/ticketswap/dbt/mart_scraped_events.parquet") }}
 
 with
-    scraped as (select * from {{ ref("int_event_scrape") }}),
-    entrance as (select * from {{ ref("int_event_entrances") }}),
+scraped as (select * from {{ ref("int_event_scrape") }}
+),
 
-    event_entrances_scraped as (
-        select
-            scraped.event_entrance_id,
-            scraped.scraped_day,
-            scraped.event_name,
-            cast(event_start_date as datetime) as event_start_date,
-            scraped.entrance_title,
-            scraped.sum_price_per_day,
-            scraped.sum_tickets_per_day,
-            scraped.avg_price_per_ticket_per_day,
-            scraped.avg_original_price_per_ticket_per_day,
-            scraped.avg_profit_per_ticket_per_day,
+entrance as (select * from {{ ref("int_event_entrances") }}
+),
 
-            entrance.scraped_days_per_entrance,
-            entrance.total_tickets_per_entrance,
-            entrance.total_price_per_entrance,
-            entrance.total_original_price_per_entrance,
-            entrance.total_profit_per_entrance,
-            entrance.avg_price_per_entrance,
-            entrance.avg_original_price_per_entrance,
-            entrance.avg_profit_per_entrance,
-            entrance.max_price_per_entrance,
-            entrance.min_price_per_entrance,
+event_entrances_scraped as (
+    select
+        scraped.event_entrance_id,
+        scraped.scraped_day,
+        scraped.event_name,
+        cast(event_start_date as datetime) as event_start_date,
+        scraped.entrance_title,
+        scraped.sum_price_per_day,
+        scraped.sum_tickets_per_day,
+        scraped.avg_price_per_ticket_per_day,
+        scraped.avg_original_price_per_ticket_per_day,
+        scraped.avg_profit_per_ticket_per_day,
 
-            entrance.price_per_entrance_stddev,
-            entrance.price_per_entrance_q1,
-            entrance.price_per_entrance_q3
+        entrance.scraped_days_per_entrance,
+        entrance.total_tickets_per_entrance,
+        entrance.total_price_per_entrance,
+        entrance.total_original_price_per_entrance,
+        entrance.total_profit_per_entrance,
+        entrance.avg_price_per_entrance,
+        entrance.avg_original_price_per_entrance,
+        entrance.avg_profit_per_entrance,
+        entrance.max_price_per_entrance,
+        entrance.min_price_per_entrance,
 
-        from scraped
-        left join entrance
-            on scraped.event_entrance_id = entrance.event_entrance_id
-        order by
-            scraped.scraped_day asc,
-            scraped.event_name,
-            scraped.event_start_date desc,
-            scraped.sum_tickets_per_day desc
-    )
+        entrance.price_per_entrance_stddev,
+        entrance.price_per_entrance_q1,
+        entrance.price_per_entrance_q3
+
+    from scraped
+    left join entrance
+        on scraped.event_entrance_id = entrance.event_entrance_id
+    order by
+        scraped.scraped_day asc,
+        scraped.event_name,
+        scraped.event_start_date desc,
+        scraped.sum_tickets_per_day desc
+)
 
 select *
 from event_entrances_scraped
