a25ef48752dde378f6504495f1f52dfdda6e263b
Lasse Benninga <devops.pipeline@example.com>
2023-03-20 19:07:56 +0100

    Add separate ints

--- models/marts/mart_scraped_events.sql
+++ models/marts/mart_scraped_events.sql
@@ -1,79 +1,16 @@
 {{ config(materialized="table") }}
-{{ config(materialized='external', format='parquet', location="s3://lbenninga-projects/ticketswap/dbt/mart_tickets_sold_stats.parquet") }}
+{{ config(materialized='external', format='parquet', location="s3://lbenninga-projects/ticketswap/dbt/mart_scraped_events.parquet") }}
 
 with
-    tickets_sold as (select * from {{ ref("stg_tickets_sold") }}),
+    scraped as (select * from {{ ref("int_event_scrape") }}),
+    entrance as (select * from {{ ref("int_event_entrances") }}),
 
-    tickets as (
-        select
-            date_trunc('day', updated) as scraped_day,
-            event_name,
-            event_start_date,
-            entrance_title,
-            event_entrance_id,
-            amount_of_tickets,
-            original_price,
-            price
-
-        from tickets_sold
-
-    ),
-
-    grouped_scraped_day_event_startdate_entrance as (
-        select
-            scraped_day,
-            event_name,
-            event_start_date,
-            entrance_title,
-            event_entrance_id,
-
-            round(sum(price)) as sum_price_per_day,
-            sum(amount_of_tickets) as sum_tickets_per_day,
-            round(sum(price) / sum(amount_of_tickets)) as avg_price_per_ticket_per_day,
-            round(
-                sum(original_price) / sum(amount_of_tickets)
-            ) as avg_original_price_per_ticket_per_day,
-            round(sum(price) / sum(amount_of_tickets)) - round(
-                sum(original_price) / sum(amount_of_tickets)
-            ) as avg_profit_per_ticket_per_day
-
-        from tickets
-        group by 1, 2, 3, 4, 5
-
-    ),
-
-    grouped_event_startdate_entrance as (
-        select
-            event_name,
-            event_start_date,
-            entrance_title,
-            event_entrance_id,
-
-            count(distinct scraped_day) as scraped_days_per_entrance,
-            sum(amount_of_tickets) as total_tickets_per_entrance,
-            round(sum(price)) as total_price_per_entrance,
-            round(sum(original_price)) as total_original_price_per_entrance,
-            round(sum(price) - sum(original_price)) as total_profit_per_entrance,
-            round(avg(price)) as avg_price_per_entrance,
-            round(avg(original_price)) as avg_original_price_per_entrance,
-            round(avg(price) - avg(original_price)) as avg_profit_per_entrance,
-            max(price) as max_price_per_entrance,
-            min(price) as min_price_per_entrance,
-
-            round(stddev_samp(price)) as price_per_entrance_stddev,
-            approx_quantile(price, 0.01) as price_per_entrance_q1,
-            approx_quantile(price, 0.99) as price_per_entrance_q3
-
-        from tickets
-        group by 1, 2, 3, 4
-    ),
-
-    final as (
+    event_entrances_scraped as (
         select
             scraped.event_entrance_id,
             scraped.scraped_day,
             scraped.event_name,
-            scraped.event_start_date,
+            cast(event_start_date as datetime) as event_start_date,
             scraped.entrance_title,
             scraped.sum_price_per_day,
             scraped.sum_tickets_per_day,
@@ -96,9 +33,8 @@ with
             entrance.price_per_entrance_q1,
             entrance.price_per_entrance_q3
 
-        from grouped_scraped_day_event_startdate_entrance scraped
-        left join
-            grouped_event_startdate_entrance entrance
+        from scraped
+        left join entrance
             on scraped.event_entrance_id = entrance.event_entrance_id
         order by
             scraped.scraped_day asc,
@@ -108,4 +44,4 @@ with
     )
 
 select *
-from final
+from event_entrances_scraped
