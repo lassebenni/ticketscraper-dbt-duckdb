84f70e93c245eb93f1bb9d6432b309b576cbe6ed
Automated Publisher <actions@users.noreply.github.com>
2023-03-30 19:40:36 +0000

    Automated publish: Thu Mar 30 19:40:36 UTC 2023 ea0aa7436f27d48227732fb3ee9f4af4f516cb82

--- models/int/int_event_entrances.sql
+++ models/int/int_event_entrances.sql
@@ -5,67 +5,67 @@ tickets_sold as (select * from {{ ref("int_validated_tickets") }}
 ),
 
 tickets as (
-  select
-    event_name,
-    event_start_date,
-    entrance_title,
-    event_entrance_id,
-    amount_of_tickets,
-    original_price,
-    price,
-    date_trunc('day', updated) as scraped_day
+    select
+        event_name,
+        event_start_date,
+        entrance_title,
+        event_entrance_id,
+        amount_of_tickets,
+        original_price,
+        price,
+        date_trunc('day', updated) as scraped_day
 
-  from tickets_sold
+    from tickets_sold
 
 ),
 
 event_entrances as (
-  select
-    event_name,
-    event_start_date,
-    entrance_title,
-    event_entrance_id,
+    select
+        event_name,
+        event_start_date,
+        entrance_title,
+        event_entrance_id,
 
-    count(distinct scraped_day) as scraped_days_per_entrance,
-    sum(amount_of_tickets) as total_tickets_per_entrance,
-    round(sum(price)) as total_price_per_entrance,
-    round(sum(original_price)) as total_original_price_per_entrance,
-    round(sum(price) - sum(original_price)) as total_profit_per_entrance,
-    round(avg(price)) as avg_price_per_entrance,
-    round(avg(original_price)) as avg_original_price_per_entrance,
-    round(avg(price) - avg(original_price)) as avg_profit_per_entrance,
-    max(price) as max_price_per_entrance,
-    min(price) as min_price_per_entrance,
-    median(price) as median_price_per_entrance,
+        count(distinct scraped_day) as scraped_days_per_entrance,
+        sum(amount_of_tickets) as total_tickets_per_entrance,
+        round(sum(price)) as total_price_per_entrance,
+        round(sum(original_price)) as total_original_price_per_entrance,
+        round(sum(price) - sum(original_price)) as total_profit_per_entrance,
+        round(avg(price)) as avg_price_per_entrance,
+        round(avg(original_price)) as avg_original_price_per_entrance,
+        round(avg(price) - avg(original_price)) as avg_profit_per_entrance,
+        max(price) as max_price_per_entrance,
+        min(price) as min_price_per_entrance,
+        median(price) as median_price_per_entrance,
 
 
-    round(stddev_samp(price)) as price_per_entrance_stddev,
-    approx_quantile(price, 0.01) as price_per_entrance_q1,
-    approx_quantile(price, 0.99) as price_per_entrance_q3
+        round(stddev_samp(price)) as price_per_entrance_stddev,
+        approx_quantile(price, 0.01) as price_per_entrance_q1,
+        approx_quantile(price, 0.99) as price_per_entrance_q3
 
-  from tickets
-  group by 1, 2, 3, 4
+    from tickets
+    group by 1, 2, 3, 4
 ),
 
 final as (
-  select
-    event_entrance_id,
-    scraped_days_per_entrance,
-    total_tickets_per_entrance,
-    total_price_per_entrance,
-    total_original_price_per_entrance,
-    total_profit_per_entrance,
-    avg_price_per_entrance,
-    avg_original_price_per_entrance,
-    avg_profit_per_entrance,
-    max_price_per_entrance,
-    min_price_per_entrance,
-    median_price_per_entrance,
-    price_per_entrance_stddev,
-    price_per_entrance_q1,
-    price_per_entrance_q3
+    select
+        event_entrance_id,
+        scraped_days_per_entrance,
+        total_tickets_per_entrance,
+        total_price_per_entrance,
+        total_original_price_per_entrance,
+        total_profit_per_entrance,
+        avg_price_per_entrance,
+        avg_original_price_per_entrance,
+        avg_profit_per_entrance,
+        max_price_per_entrance,
+        min_price_per_entrance,
+        median_price_per_entrance,
+        price_per_entrance_stddev,
+        price_per_entrance_q1,
+        price_per_entrance_q3
 
-  from event_entrances
+    from event_entrances
 )
 
 select *
