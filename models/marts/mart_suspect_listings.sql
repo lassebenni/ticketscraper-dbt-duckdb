ea0aa7436f27d48227732fb3ee9f4af4f516cb82
Lasse Benninga <devops.pipeline@example.com>
2023-03-30 21:34:00 +0200

    Refactor how ticket prices are evaluated in the ticket validation model. - Simplifies how the price is calculated and reduces queries needed. - Updates the event entrances model to aggregate data on ticket sales. - Updates the suspect listings mart to identify price outliers by comparing with median ticket price.

--- models/marts/mart_suspect_listings.sql
+++ models/marts/mart_suspect_listings.sql
@@ -1,46 +1,50 @@
 {{ config(materialized="table") }}
+
 {{ config(materialized='external', format='parquet', location="s3://lbenninga-projects/ticketswap/dbt/mart_suspect_listings.parquet") }}
 
 with
-tickets as (select * from {{ ref("int_tickets_enriched") }}
+tickets as (
+    select * from {{ ref("int_tickets_enriched") }}
 ),
-
-entrances as (select * from {{ ref("int_event_entrances") }}
+entrances as (
+    select * from {{ ref("int_event_entrances") }}
 ),
-
-invalid_listings as (select * from {{ ref("stg_invalid_listings") }}
+invalid_listings as (
+    select * from {{ ref("stg_invalid_listings") }}
 ),
-
 event_stdev as (
     select
         event_entrance_id,
         total_tickets_per_entrance,
-        round(price_per_entrance_q1 - 1.5 * (price_per_entrance_q3 - price_per_entrance_q1)) as price_lower_bound,
-        round(price_per_entrance_q3 + 1.5 * (price_per_entrance_q3 - price_per_entrance_q1)) as price_upper_bound
+        {# round(price_per_entrance_q1 / 1.5) as price_lower_bound,
+        round(price_per_entrance_q3 * 1.5) as price_upper_bound, #}
+        round(median_price_per_entrance / 2) as price_lower_bound,
+        round(median_price_per_entrance * 2) as price_upper_bound,
+        median_price_per_entrance,
+
     from entrances
 ),
-
 suspect_event as (
-
     select
-        t.id,
         t.updated,
+        t.id,
         t.event_name,
         t.event_start_date,
         t.entrance_title,
         t.price,
+        median_price_per_entrance,
+        abs(round((t.price - e.median_price_per_entrance) / e.median_price_per_entrance * 100)) as price_diff_median_percent,
+        e.price_lower_bound,
+        e.price_upper_bound,
         t.original_price,
         t.url,
-        case
-            when t.price > e.price_upper_bound or t.price < e.price_lower_bound
-                then 1
-            else 0
-        end as suspect,
         e.event_entrance_id,
         e.total_tickets_per_entrance,
-        e.price_lower_bound,
-        e.price_upper_bound
-
+        case
+            when t.price > e.price_upper_bound or t.price < e.price_lower_bound
+            then 1
+            else 0
+        end as suspect
     from tickets t
     left join event_stdev e on t.event_entrance_id = e.event_entrance_id
     where id not in (select id from invalid_listings)
@@ -49,3 +53,4 @@ suspect_event as (
 select *
 from suspect_event
 where suspect = 1
+order by price_diff_median_percent asc
