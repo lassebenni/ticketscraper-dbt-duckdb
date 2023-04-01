06d71512d492aba2274d4bd20a1f2515b6329212
Lasse Benninga <devops.pipeline@example.com>
2023-04-01 20:29:08 +0200

    Write events to s3

--- models/staging/stg_events.sql
+++ models/staging/stg_events.sql
@@ -1,5 +1,5 @@
 {{ config(materialized="incremental", unique_key="id") }}
-{{ config(materialized='external', format='parquet', location="events.parquet") }}
+{{ config(materialized='external', format='parquet', location="s3://lbenninga-projects/ticketswap/dbt/stg_events.parquet") }}
 
 with
 source as (select * from {{ source("events", "events") }}
@@ -7,12 +7,13 @@ source as (select * from {{ source("events", "events") }}
 
 select
     updated,
-    id,
+    id as event_id,
     name,
     entrance_slug as slug,
     unnest(entrance_types).available_tickets as entrance_available_tickets,
     unnest(entrance_types).slug as entrance_slug,
     unnest(entrance_types).title as entrance_title,
+    unnest(entrance_types).id as entrance_id,
     available_tickets,
     sold_tickets,
     wanted_tickets,
