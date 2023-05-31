d1b7ed660a2b03305c4423bade4367a9c93c998f
Lasse Benninga <devops.pipeline@example.com>
2023-06-01 00:25:35 +0200

    add checked tickets

---
+++ models/int/checked_tickets.py
@@ -0,0 +1,40 @@
+import requests
+import time
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0",
+    "Accept": "*/*",
+    "Accept-Language": "en",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Referer": "https://www.ticketswap.com/",
+    "content-type": "application/json",
+    "Origin": "https://www.ticketswap.com",
+    "Connection": "keep-alive",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-site",
+    "TE": "trailers",
+}
+
+
+def check_expired(url: str) -> bool:
+    response = requests.get(url, headers=headers)
+    time.sleep(1)
+
+    if "Bummer, this type of tickets has" in response.text:
+        return True
+    elif "<strong>sold" in response.text:
+        return False
+    else:
+        raise ValueError("Unexpected response from TicketSwap")
+
+
+def model(dbt, session):
+    dbt.config(materialized="table")
+    rel = dbt.ref("int_suspect_listings")
+
+    df = rel.to_df()
+    # for each row in the df check if the 'url' is expired
+    df["expired"] = df["url"].apply(lambda x: check_expired(x))
+
+    return df
