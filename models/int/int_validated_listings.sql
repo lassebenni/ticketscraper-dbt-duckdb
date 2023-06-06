4101d82cee0b18b490c7f5644e6bd696aacf866f
Automated Publisher <actions@users.noreply.github.com>
2023-06-06 13:33:50 +0000

    Automated publish: Tue Jun  6 13:33:50 UTC 2023 9023d79fdc388988f7f2712336c843eeab66b97d

--- models/int/int_validated_listings.sql
+++ models/int/int_validated_listings.sql
@@ -1,38 +1,38 @@
 {{ config(materialized="table") }}
 
 with
-    listings_sold as (select * from {{ ref("stg_listings_sold") }}),
+listings_sold as (select * from {{ ref("stg_listings_sold") }}),
 
-    invalid_listings as (select * from {{ ref("stg_invalid_listings") }}),
+invalid_listings as (select * from {{ ref("stg_invalid_listings") }}),
 
-    expired_listings as (select * from {{ ref("int_expired_listings") }}),
+expired_listings as (select * from {{ ref("int_expired_listings") }}),
 
-    listings_sold_clean as (
-        select
-            id,
-            description,
-            event_name,
-            event_start_date,
-            event_end_date,
-            amount_of_listings,
-            entrance_title,
-            entrance_id,
-            original_price,
-            price,
-            currency,
-            location,
-            city,
-            url,
-            status,
-            seller_name,
-            seller_id,
-            event_entrance_id,
-            updated,
-        from listings_sold
-        where
-            id not in (select id from invalid_listings)
-            and id not in (select id from expired_listings)
-    )
+listings_sold_clean as (
+    select
+        id,
+        description,
+        event_name,
+        event_start_date,
+        event_end_date,
+        amount_of_listings,
+        entrance_title,
+        entrance_id,
+        original_price,
+        price,
+        currency,
+        location,
+        city,
+        url,
+        status,
+        seller_name,
+        seller_id,
+        event_entrance_id,
+        updated
+    from listings_sold
+    where
+        id not in (select id from invalid_listings)
+        and id not in (select id from expired_listings)
+)
 
 select *
 from listings_sold_clean
